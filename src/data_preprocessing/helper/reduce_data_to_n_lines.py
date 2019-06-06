a27419d617ee77aca7223704e040fed511270f46
Brunner Ursin <ursin.brunner@ti8m.ch>
2019-06-06 10:39:11 +0200

    a little helper to break big files down (just for initial tests)

---
+++ src/data_preprocessing/helper/reduce_data_to_n_lines.py
@@ -0,0 +1,28 @@
+import csv
+import logging
+
+from logging_customized import setup_logging
+
+setup_logging()
+
+
+def write_first_n_lines_of_file(path_src, path_dest, n_lines):
+
+    with open(path_src) as src, open(path_dest, "w+") as dest:
+
+        reader = csv.reader(src, delimiter=",")
+        writer_dest = csv.writer(dest, delimiter=',')
+
+        for idx, line in enumerate(reader):
+            if idx < n_lines:
+                writer_dest.writerow(line)
+            else:
+                break
+
+        logging.info("created new CSV ({}) with first {} lines of {}.".format(path_dest, n_lines, path_src))
+
+
+if __name__ == "__main__":
+    write_first_n_lines_of_file("data/company/deep_matcher/train.large.csv", "data/company/deep_matcher/train.csv", 1000)
+    write_first_n_lines_of_file("data/company/deep_matcher/test.large.csv", "data/company/deep_matcher/test.csv", 1000)
+    write_first_n_lines_of_file("data/company/deep_matcher/valid.large.csv", "data/company/deep_matcher/valid.csv", 1000)
