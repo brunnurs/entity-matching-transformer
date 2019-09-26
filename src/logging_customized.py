37c268607e2acd02770afa87dc130a699e31aa0f
Brunner Ursin <ursin.brunner@ti8m.ch>
2019-09-26 18:23:32 +0200

    Attach standard-out to logger so we can pipe the whole output to a file.

--- src/logging_customized.py
+++ src/logging_customized.py
@@ -1,9 +1,11 @@
 import logging
+import sys
 
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO,
                         format='%(message)s',
-                        datefmt="%H:%M:%S")
+                        datefmt="%H:%M:%S",
+                        stream=sys.stdout)
 
     logging.getLogger('bert-classifier-entity-matching')
